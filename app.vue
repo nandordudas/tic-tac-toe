8a1fb119d5f72bf24692ea567f8b73713409556d
Nándor Dudás <nandor.dudas@gmail.com>
2024-05-19 17:52:36 +0200

    chore: release v0.0.1

--- app.vue
+++ app.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 const isDev = import.meta.dev
+const faviconHref = isDev ? '/favicon-dev.svg' : '/favicon.svg'
 
 const logger = useLogger('app.vue')
 const { name, version } = useRuntimeConfig().public.app
 
 useHead({
-  link: [{ rel: 'icon', type: 'image/svg+xml', href: isDev ? '/favicon-dev.svg' : '/favicon.svg' }],
+  link: [{ rel: 'icon', type: 'image/svg+xml', href: faviconHref }],
   titleTemplate: title => isDev ? `${title} · ${name}@${version}` : `${title} · TicTacToe`,
 })
 
